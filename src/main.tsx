fef5a9dc71b2ca82bb7fb3f8e36c197fc03d374b
wuchenxi <843448525@qq.com>
2026-06-22 01:39:37 +0800

    fix router basename

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ if (saved) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>
