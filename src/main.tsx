3a6d07b17b117c6ebc254113ba68842b499d2278
wuchenxi <843448525@qq.com>
2026-06-24 21:35:03 +0800

    update June 22 results: ARG 2-0 AUT, FRA 3-0 IRQ, NOR 3-2 SEN, JOR 1-2 ALG

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -17,8 +17,8 @@ if (saved) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
